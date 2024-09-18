6296afcd43963de340303db69d19e1c67f88e3b8
Konstantinos Tsouvalis <ktsouvalis@uop.gr>
2024-09-18 06:01:18 +0000

    change title in confirm.blade.php

--- resources/views/confirm.blade.php
+++ resources/views/confirm.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Upload Emails</title>
+    <title>Confirm and Send Emails</title>
 </head>
 <body>
     @if(session('non_emails'))
